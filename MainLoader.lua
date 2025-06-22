56b249e984cfd124ca8b7f6d1ce9fae768ede08e
gamerscripter90 <talgarne8@gmail.com>
2025-06-22 11:28:04 +0800

    Delete MainLoader.lua
    Nuh uh

--- MainLoader.lua
+++
@@ -1,7 +0,0 @@
--- MainLoader.lua
-
--- Load UI Tabs and Main Content
-local tabsContainer, mainContent = loadstring(game:HttpGet("https://paste.ee/r/HH8s8fLW"))()
-
--- Load Features Script with UI references
-loadstring(game:HttpGet("https://paste.ee/r/22jWbx63"))(tabsContainer, mainContent)
